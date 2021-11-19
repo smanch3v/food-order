ddc4bc9c84656c0c579087bb2ee576ebf6c145a2
smanch3v <svetoslavmanchev@gmail.com>
2021-11-19 14:44:05 +0100

    format

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import CartIcon from '../Cart/CartIcon';
-import CartContext from '../store/cart-context';
-import classes from './HeaderCartButton.module.css';
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../store/cart-context";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
